ab54f077bb2d0e15d60b31d8bf0289f717c3b0df
decanus <7621705+decanus@users.noreply.github.com>
2021-01-28 02:06:08 +0100

    added some more

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'ion' do
+  use_frameworks!
+
+  pod 'GoogleWebRTC'
+
+end
---
+++ Sources/ion/WebRTC.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// Role represents whether a transport is the publisher or subscriber.
+enum Role {
+    case publisher
+    case subscriber
+}
